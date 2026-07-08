cd8c4a3885d9c87d12e6cc03cf915ebb96839663
Tyler <3deathsaves@gmail.com>
2026-07-08 17:27:32 -0600

    Warding Flare Fixes.

--- scripts/macros/modern/classFeatures/cleric/light/improvedWardingFlare.mjs
+++ scripts/macros/modern/classFeatures/cleric/light/improvedWardingFlare.mjs
@@ -2,17 +2,10 @@ import {actorUtils, documentUtils} from '../../../../../proxy.mjs';
 async function added({document}) {
     const wardingFlare = actorUtils.getItemByIdentifier(document.actor, 'warding-flare');
     if (!wardingFlare) return;
-    const updates = {};
-    if (!wardingFlare.system.uses.recovery.find(i => i.period === 'sr')) {
-        const recovery = wardingFlare.toObject().system.uses.recovery;
-        recovery.push({formula: undefined, period: 'sr', type: 'recoverAll'});
-        updates['system.uses.recovery'] = recovery;
-    }
-    const rollMacros = wardingFlare.flags.cat?.macros?.roll ?? [];
-    if (!rollMacros.some(i => i.source === 'chris-premades' && i.identifier === 'warding-flare')) {
-        updates['flags.cat.macros.roll'] = [...rollMacros, {source: 'chris-premades', rules: '2024', identifier: 'warding-flare'}];
-    }
-    if (Object.keys(updates).length) await documentUtils.update(wardingFlare, updates);
+    if (wardingFlare.system.uses.recovery.find(i => i.period === 'sr')) return;
+    const recovery = wardingFlare.toObject().system.uses.recovery;
+    recovery.push({formula: undefined, period: 'sr', type: 'recoverAll'});
+    await documentUtils.update(wardingFlare, {'system.uses.recovery': recovery});
 }
 export const improvedWardingFlare = {
     name: 'Improved Warding Flare',
