11267ab84ef887ed32c438bebcab6a1f58bf2229
bacon-nugget <122384608+bacon-nugget@users.noreply.github.com>
2026-07-16 18:07:12 -0400

    brutal strike fixes
    - consume per turn charge on hit or miss
    - use token documents in completeActivityUse

--- scripts/macros/modern/classFeatures/barbarian/brutalStrike.mjs
+++ scripts/macros/modern/classFeatures/barbarian/brutalStrike.mjs
@@ -1,12 +1,12 @@
 import {actorUtils, automationUtils, combatUtils, dialogUtils, queryUtils, rollUtils, tokenUtils, workflowUtils} from '../../../../proxy.mjs';
-async function preAttack({document: item, identifier, workflow}) {
-    if (!item.system.uses.value) return;
+async function preAttack({document: activity, identifier, workflow}) {
+    if (!activity.item.system.uses.value) return;
     if (workflow.activity.ability !== 'str') return;
     if (workflow.disadvantage || !workflow.advantage) return;
     if (!combatUtils.isOwnTurn(workflow.token) || !workflowUtils.isAttackType(workflow, 'attack')) return;
     const effect = actorUtils.getEffectByIdentifier(workflow.actor, 'recklessAttackEffect');
     if (!effect || !effect.active) return;
-    const activities = item.system.activities.contents;
+    const activities = activity.item.system.activities.contents.filter(a => a.id !== activity.id);
     const data = Object.create(Object.prototype, {
         activities: {get: () => activities, enumerable: true},
         workflow: {value: workflow, enumerable: true}
@@ -14,39 +14,40 @@ async function preAttack({document: item, identifier, workflow}) {
     await automationUtils.calledEvent('brutalStrike', workflow.actor, {canOverlap: true, multiResult: true, data});
     const choices = activities.filter(a => a instanceof dnd5e.dataModels.activity.BaseActivityData);
     if (!choices?.length) return;
-    const config = automationUtils.getConfigValues(item, ['count', 'damage']);
-    config.count = (await rollUtils.rollDice(config.count, {document: item})).total;
+    const config = automationUtils.getConfigValues(activity.item, ['count', 'damage']);
+    config.count = (await rollUtils.rollDice(config.count, {document: activity.item})).total;
     const options = {addNoneDocument: true, checkbox: true, max: config.count};
-    const choice = await dialogUtils.selectDocumentDialog(item.name, _loc('CHRISPREMADES.Macros.Modern.BrutalStrike.Use', {item: item.name}), choices, options);
-    const chosen = [];
+    const choice = await dialogUtils.selectDocumentDialog(activity.item.name, _loc('CHRISPREMADES.Macros.Modern.BrutalStrike.Use', {item: activity.item.name}), choices, options);
     if (!choice) return;
+    const chosen = [];
     if (config.count > 1) {
         for (const result of choice)
             if (result.amount) chosen.push(result.document);
     }
     else chosen.push(choice);
     if (!chosen.length) return;
+    await workflowUtils.completeActivityUse(activity);
     workflowUtils.setWorkflowProperty(workflow, 'brutalStrikesActivities', chosen);
     workflowUtils.setWorkflowProperty(workflow, 'brutalStrikesDamage', config.damage);
-    workflow.tracker.advantage.suppress(identifier, item.name);
+    workflow.tracker.advantage.suppress(identifier, activity.item.name);
 }
 async function damage({workflow}) {
     const formula = workflowUtils.getWorkflowProperty(workflow, 'brutalStrikesDamage');
-    if (!formula) return;
+    if (!formula || !workflow.hitTargets.size) return;
     await workflowUtils.bonusDamage(workflow, formula);
 }
 async function doStrike({workflow}) {
-    if (!workflow.hitTargets.size) return;
     const activities = workflowUtils.getWorkflowProperty(workflow, 'brutalStrikesActivities');
-    if (!activities) return;
-    for (let i = 0; i < activities.length; i++) 
-        await workflowUtils.completeActivityUse(activities[i], workflow.hitTargets, {consumeResources: i === 0});
+    if (!activities || !workflow.hitTargets.size) return;
+    const targets = Array.from(workflow.hitTargets.map(i => i.document));
+    for (let i = 0; i < activities.length; i++)
+        await workflowUtils.completeActivityUse(activities[i], targets);
 }
 async function forcefulBlowPush({document: activity, workflow}) {
     const distanceFormula = automationUtils.getConfigValue(activity.item, 'push');
     const distance = (await rollUtils.rollDice(distanceFormula, {document: activity})).total;
-    for (const token of workflow.hitTargets)
-        await tokenUtils.slideToken(token.document, {sourceToken: workflow.token, distance});
+    for (const token of workflow.targets)
+        await tokenUtils.slideToken(token.document, {sourceToken: workflow.token.document, distance});
 }
 export const brutalStrike = {
     name: 'Brutal Strike',
