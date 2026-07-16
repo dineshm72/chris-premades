3b93690477b0e42043c9764cf56aa7e1d6ae8f2d
bacon-nugget <122384608+bacon-nugget@users.noreply.github.com>
2026-07-16 00:29:14 -0400

    Update all.mjs

--- scripts/macros/all.mjs
+++ scripts/macros/all.mjs
@@ -2,4 +2,3 @@
 export {instinctivePounce as 'instinctive-pounce'} from './all/classFeatures/barbarian/instinctivePounce.mjs';
 export {persistentRage as 'persistent-rage'} from './all/classFeatures/barbarian/persistentRage.mjs';
 export {rage, raging} from './all/classFeatures/barbarian/rage.mjs';
-export {unarmoredDefense as 'unarmored-defense'} from './all/classFeatures/barbarian/unarmoredDefense.mjs';
